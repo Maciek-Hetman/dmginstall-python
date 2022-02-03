e3b06e8f2c9fcaae548d1292485410d46b92b851
Maciek-Hetman <maciekhetman577@gmail.com>
2022-02-03 20:46:01 +0100

    update update update

--- update.py
+++ update.py
@@ -3,14 +3,5 @@ from operator import ge
 import subprocess
 import os
 
-def getCommandOutput(command):
-    subprocess.check_output(command, shell=True, universal_newlines=True)
-
-if getCommandOutput('which dmginstall') == '':
-    os.system('cp dmginstall.py /usr/local/bin/dmginstall')
-else:
-    os.system('rm $(which dmginstall)')
-    os.system('cp dmginstall.py /usr/local/bin/dmginstall')
-    if getCommandOutput('which vcp') == '':
-        os.system('rm $(which vcp)')
-        os.system('cp vcp/vcp /usr/local/bin/vcp') 
+os.system('cp dmginstall.py /usr/local/bin/dmginstall')
+os.system('cp vcp/vcp /usr/local/bin/vcp')
